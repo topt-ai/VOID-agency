4ced7c7815434a0ddd70ad076fbecb4e3af20c4d
topt-ai <ricardoacevedo1199@gmail.com>
2026-03-08 17:28:26 -0600

    Delete void-studio directory

--- void-studio/src/components/CustomCursor.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useCursor } from "../hooks/useCursor";
-import styles from "./CustomCursor.module.css";
-
-export function CustomCursor() {
-  const cursorRef = useCursor();
-
-  return <div ref={cursorRef} className={styles.cursor} />;
-}
